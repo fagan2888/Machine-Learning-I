be1afacc0f3fc0f98858edc5a0644f98b4be5684
Matt Wilchek <mwilchek@gmu.edu>
2018-09-20 22:30:05 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+# For Reference go to:
+# https://github.com/rasbt/python-machine-learning-book-2nd-edition/blob/master/code/ch03/ch03.ipynb
+
 # Load the data
 df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data', header=None)
 df.columns = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'target']
